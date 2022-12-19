b35dbb8a2af11ee1150e8121f0750055e7c8d231
BuildTools <unconfigured@null.spigotmc.org>
2022-12-19 14:24:42 +0100

    optimize imports

--- libs/creators/src/lib/+state/creators/creators.effects.ts
+++ libs/creators/src/lib/+state/creators/creators.effects.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, defer, map, of, switchMap } from 'rxjs';
+import { catchError, map, of, switchMap } from 'rxjs';
 import { CreatorService } from '../../creator.service';
 
 import * as CreatorsActions from './creators.actions';
